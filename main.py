7508b0454a958cf5061c11e4b3ab4e0c4686c7e7
Erin Grasmick <egrasmick@wisc.edu>
2018-04-14 17:11:22 +0200

    working for multiple directories of data organized by year as found on the ftp

--- main.py
+++ main.py
@@ -10,22 +10,25 @@ client = InfluxDBClient(host='127.0.0.1', port=8086, database='noaa')
 pp = pprint.PrettyPrinter(indent=4)
 DIR = "/home/leaf/Downloads/noaa/"
 
-def send_to_influx(data):
+def send_dict_to_influx(data):
     for station in data:
         metrics = {}
         metrics['measurement'] = "min_avg_temp"
         metrics['tags'] = {}
         metrics['fields'] = {}
         metrics['tags']['station_name'] = data[station]["STATION NAME"]
+        if not data[station]["GEOHASH"]:
+            continue
         metrics['tags']['geohash'] = data[station]["GEOHASH"]
         metrics['tags']['country'] = data[station]["CTRY"]
         metrics['tags']['usaf_code'] = data[station]['USAF']
         if data[station]["ST"]:
             metrics['tags']['state'] = data[station]["ST"]
         for year in data[station]["TEMPS"].keys():
-            metrics['time'] = year + "-01-01T00:00:00Z00:00"
+            metrics['time'] = year + "-12-31T00:00:00Z00:00"
             metrics['fields']["value"] = float(data[station]["TEMPS"][year])
-        client.write_points([metrics])
+        print(metrics)
+        #client.write_points([metrics])
 
 def get_station_info(usaf_num):
     station_dict = {}
@@ -61,59 +64,64 @@ def divide_station_line(line):
 
 def parse_data_date(line):
     ymd = line[13:21]
-    return ymd
+    hm = line[21:25]
+    return ymd, hm
+
+def parse_data_temp(line):
+    temp = int(line[83:88], 16)
+    return temp
 
 
 info_list = []
 infodict = {}
-for filename in os.listdir(DIR+"2017/2017out/"):
-    if os.path.isdir(DIR + "2017/2017out/" + filename):
-       # print(filename, "is a directory, skipping...")
-        continue
-    with open(DIR + "2017/2017out/" + filename) as file:
-        data = file.readlines()[1:]
-        first = True
-        prev_date = ""
-        all_temps = []
-        for line in data:
-            date = parse_data_date(line)
-            if first:
-                prev_date = date
-                first = False
-                day_temps = []
-            if date == prev_date:
-                # check to see if there's a valid temperature in the line
-                try:
-                    temp = int(line[83:88], 16)
-                except ValueError:
-                    continue
-                # intake another line once we got the temperature
-                # check if we've reached the end of the file yet
-                day_temps.append(temp)
-                # sort all the temperatures from the day we've collected
-            else:
-                # keep the three lowest temps
-                day_temps.sort()
-                coldest_day_temps = day_temps[0:3]
-                day_temps = []
-                if any(coldest_day_temps):
-                    all_temps.append(mean(coldest_day_temps))
-                # add the mean day temperature to the list of all day temps
-                first = True
-        # sort all the year's daily temperatures
-        all_temps.sort()
-        # if there's not any reported temperatures, we can skip this file
-        if not any(all_temps):
-            break
-        # get a week's worth of coldest temps
-        min_avg_temp = mean(all_temps[0:7])
+directories = [x[1] for x in os.walk(DIR)][0]
+for directory in directories:
+    for filename in os.listdir(DIR + directory + "/"):
+        if os.path.isdir(DIR + directory + filename) or not filename.endswith("out"):
+            continue
+        with open(DIR + directory + "/" + filename) as file:
+            data = file.readlines()[1:]
+            first = True
+            prev_date = ""
+            all_temps = []
+            for line in data:
+                date, _ = parse_data_date(line)
 
-        station_info = filename.strip('.out').split('-')
-        station_info.append(min_avg_temp)
+                if first:
+                    prev_date = date
+                    first = False
+                    day_temps = []
+                if date == prev_date:
+                    # check to see if there's a valid temperature in the line
+                    try:
+                        temp = parse_data_temp(line)
+                    except ValueError:
+                        continue
+                    # intake another line once we got the temperature
+                    # check if we've reached the end of the file yet
+                    day_temps.append(temp)
+                    # sort all the temperatures from the day we've collected
+                else:
+                    # keep the three lowest temps
+                    day_temps.sort()
+                    coldest_day_temps = day_temps[0:3]
+                    day_temps = []
+                    if any(coldest_day_temps):
+                        all_temps.append(mean(coldest_day_temps))
+                    # add the mean day temperature to the list of all day temps
+                    first = True
+            # sort all the year's daily temperatures
+            all_temps.sort()
+            # if there's not any reported temperatures, we can skip this file
+            if not any(all_temps):
+                break
+            # get a week's worth of coldest temps
+            min_avg_temp = mean(all_temps[0:7])
 
-        infodict[station_info[0]] = {"TEMPS": {station_info[2]: station_info[3]}}
+            station_info = filename.strip('.out').split('-')
+            station_info.append(min_avg_temp)
+            infodict[station_info[0]] = {"TEMPS": {station_info[2]: station_info[3]}}
 
-#infodict = {}
 tempdict = {}
 for key in infodict.keys():
     tempdict = get_station_info(key)
@@ -130,8 +138,10 @@ for key in infodict.keys():
     if lat and lon:
         geohash = pygeohash.encode(lat, lon)
         tempdict["GEOHASH"] = geohash
+    else:
+        tempdict["GEOHASH"] = False
     for k, v in infodict[key].items():
         tempdict[k] = v
     infodict[key] = tempdict
 
-send_to_influx(infodict)
+send_dict_to_influx(infodict)
